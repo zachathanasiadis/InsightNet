310033f905aa91b0248cd8b76eb2196ac1aca5fd
Zacharias Athanasiadis <127743015+zachathanasiadis@users.noreply.github.com>
2026-05-18 12:14:52 +0200

    small change to test tf workflow

--- terraform/aca/variables.tf
+++ terraform/aca/variables.tf
@@ -5,5 +5,5 @@ variable "location" {
 
 variable "github_sp_object_id" {
   type        = string
-  description = "Microsoft Entra ID service principal object ID for the GitHub Actions OIDC identity, used for RBAC role assignments"
+  description = "Microsoft Entra ID service principal object ID for the GitHub Actions OIDC identity, used for Azure RBAC role assignments"
 }
